fb0cb93e4ab38a09bcd6779766e2b466081a3727
Ruben Maher <ruben@maher.fyi>
2018-09-02 06:12:14 +0930

    0.16.0 -> 0.16.2

--- default.nix
+++ default.nix
@@ -1,33 +1,47 @@
-{ stdenv, electron, pkgs, system, nodejs, makeDesktopItem }:
+{ stdenv
+, electron
+, fetchurl
+, makeDesktopItem
+, nodejs
+, pkgs
+, riot-web
+, system
+}:
+
 let
-nodePackages = import ./node { inherit pkgs system nodejs; };
-riot-web = nodePackages."riot-web-file:../riot-web".override (attrs: {
-  postInstall = ''
-    echo "building..."
-    npm run build
-    echo '${attrs.version}' > "webapp/version"
-  '';
-});
-desktopItem = makeDesktopItem {
-  name = "riot";
-  desktopName = "Riot";
-  genericName = "Matrix Client";
-  exec = "riot";
-  comment = "A feature-rich client for Matrix.org";
-  icon = "riot";
-  categories = "Network;InstantMessaging;Chat;";
-  extraEntries = ''
-    StartupWMClass="Riot"
-  '';
-};
+  desktopItem = makeDesktopItem {
+    name = "riot";
+    desktopName = "Riot";
+    genericName = "Matrix Client";
+    exec = "riot";
+    comment = "A feature-rich client for Matrix.org";
+    icon = "riot";
+    categories = "Network;InstantMessaging;Chat;";
+    extraEntries = ''
+      StartupWMClass="Riot"
+    '';
+  };
+
+  webApp = riot-web.overrideAttrs (attrs: rec {
+    name= "riot-web-${version}";
+    version = "0.16.2";
+
+    src = fetchurl {
+      url = "https://github.com/vector-im/riot-web/releases/download/v${version}/riot-v${version}.tar.gz";
+      sha256 = "14k8hsz2i1nd126jprvi45spdxawk4c8nb3flkrg7rmjdp5sski2";
+    };
+  });
+
+  electronApp = (import ./node { inherit pkgs system nodejs; })."riot-web-file:../riot-web/electron_app";
+
 in stdenv.mkDerivation {
-  name = "riot-desktop-${riot-web.version}";
-  inherit (riot-web) version;
+  name = "riot-desktop-${webApp.version}";
+  inherit (webApp) version;
 
   buildCommand = ''
     mkdir -p "$out/share/riot"
-    ln -s '${riot-web}/lib/node_modules/riot-web/webapp' "$out/share/riot/webapp"
-    cp -r '${nodePackages."riot-web-file:../riot-web/electron_app"}/lib/node_modules/riot-web/' "$out/share/riot/electron"
+    ln -s '${webApp}' "$out/share/riot/webapp"
+    cp -r '${electronApp}/lib/node_modules/riot-web/' "$out/share/riot/electron"
 
     for i in 16 24 48 64 96 128 256 512; do
       mkdir -p "$out/share/icons/hicolor/''${i}x''${i}/apps"
